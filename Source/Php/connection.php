93cc961ebee48aae837108fd4c789624e13e67f7
vigneshk24082000@gmail.com <vigneshk24082000@gmail.com>
2023-10-30 20:28:25 +0530

    new

--- Source/Php/connection.php
+++ Source/Php/connection.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "Hello Vignesh";
+$hostname="127.0.0.1";
+$username="root";
+$password="root";
+$dbname="test";
+
+$conn = mysqli_connect($hostname,$username,$password,$dbname);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
--- Source/scripts/RegisterValidation.js
+++ Source/scripts/RegisterValidation.js
@@ -1,30 +1,53 @@
-$(document).ready(function(){
-  
-  $("#sub").click(function(){
-    let name=$("#fn").val().trim();
-    let email=$("#em").val();
-    let dob=$("#db").val();
-    
-    
-    if(!name){
+$(document).ready(function() {
+
+  $("#sub").click(function() {
+    let name = $("#fn").val().trim();
+    let email = $("#em").val().trim();
+    let dob = $("#db").val();
+    let phone = $("#ph").val().trim();
+    let new_pass = $("#ip").val().trim();
+    let con_pass = $("#cp").val().trim();
+
+
+    if (!name) {
       alert("Name should not be empty");
       return;
     }
-    
-    if(!email){
+
+    if (!email) {
       alert("Email should not be empty");
       return;
     }
-    
-    if(!dob){
+
+    if (!dob) {
       alert("DOB should not be empty");
+      return;
+    }
+ 
+      let dob1 = new Date(dob);
+      let currentDate = new Date();
+      let age = currentDate.getFullYear() - dob1.getFullYear();
+      $("#ag").val(age);
+
+    if (!phone) {
+      alert("Phone number should not be empty");
+      return;
+    }
+
+    if (!new_pass) {
+      alert("New password should not be empty");
+      return;
+    }
+
+    if (!con_pass) {
+      alert("Confirm password should not be empty ");
+      return;
     }
     
-    let dob1=new Date(dob);
-    let currentDate=new Date();
-    let age=currentDate.getFullYear() - dob1.getFullYear();
-    $("#ag").val(age)
-   
+    if(new_pass!==con_pass){
+      alert("Confirm password and password are mismatch");
+    }
+
   });
-  
+
 });
